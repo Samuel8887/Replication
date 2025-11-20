b771a69c2884fe0199db437580d89e9532a5ed94
Samuel8887 <slma@itu.dk>
2025-11-20 12:06:50 +0100

    yaaya
    Co-authored-by: akdy1234 <akdy1234@users.noreply.github.com>

--- Chit-Chat/Server/server.go
+++ Chit-Chat/Server/server.go
@@ -28,19 +28,24 @@ type Server struct {
 	clientsMu sync.Mutex
 
 	lTime int64
-}
+	curretHighestBid int64
 
-func (s *Server) tickSeen(remote int64) int64 {
-	if remote > s.lTime {
-		s.lTime = remote
-	}
-	s.lTime++
-	return s.lTime
 }
 
 func main() {
+	lTime = 0
+	CurrentHighestBid = 0
+	
 	server := &Server{}
 	server.StartServer()
+
+
+	for {		
+		time.Sleep(5 * time.Second)
+		s.lTime++
+		log.Printf("Heartbeat sent with logical time %d", s.lTime)
+	}
+
 }
 
 func (s *Server) StartServer() {
@@ -70,107 +75,14 @@ func (s *Server) StopServer() {
 	log.Println("Server stopped")
 }
 
-func (s *Server) broadcast(msg *proto.ChatMessage, excludeID string) {
-	s.clientsMu.Lock()
-	clientsCopy := make([]client, len(s.clients))
-	copy(clientsCopy, s.clients)
-	s.clientsMu.Unlock()
-	for _, c := range clientsCopy {
-		if c.id == excludeID {
-			continue
-		}
-		go func(c client) {
-			if err := c.stream.Send(msg); err != nil {
-				log.Printf("Failed to send to %s: %v", c.id, err)
-				s.removeClient(c.id)
-			}
-		}(c)
-	}
+
+func (s* Server) Bid(ctx context.Context, req *proto.Bid) (*proto.Ack, error) {
+
+	return &proto.Ack{Succes: true, LogicalTime: s.lTime}, nil
 }
 
-func (s *Server) removeClient(id string) {
-	s.clientsMu.Lock()
-	defer s.clientsMu.Unlock()
-	for i, c := range s.clients {
-		if c.id == id {
-			s.clients = append(s.clients[:i], s.clients[i+1:]...)
-			break
-		}
-	}
+func (s* Server) Result(ctx context.Context, req *proto.Result) (*proto.CurrentBid, error) {
+
+	return &proto.CurrentBid{Money: s.curretHighestBid, LogicalTime: s.lTime}, nil
 }
 
-func (s *Server) Join(req *proto.JoinRequest, stream proto.Chit_Chat_JoinServer) error {
-	s.clientsMu.Lock()
-	s.clients = append(s.clients, client{id: req.ClientId, stream: stream})
-	s.clientsMu.Unlock()
-
-	if req.LogicalTime > s.lTime {
-		s.lTime = req.LogicalTime
-	}
-	s.lTime++
-
-	s.broadcast(&proto.ChatMessage{
-		From:        "server",
-		Message:     fmt.Sprintf("%s has joined the chat!", req.ClientId),
-		LogicalTime: s.lTime,
-	}, req.ClientId)
-
-	select {}
-}
-
-func (s *Server) Publish(ctx context.Context, req *proto.PublishRequest) (*proto.Ack, error) {
-	s.clientsMu.Lock()
-	defer s.clientsMu.Unlock()
-
-	if req.LogicalTime > s.lTime {
-		s.lTime = req.LogicalTime
-	}
-	s.lTime++
-
-	msg := &proto.ChatMessage{
-		From:        req.ClientId,
-		Message:     req.Content,
-		LogicalTime: s.lTime,
-	}
-
-	for i := 0; i < len(s.clients); i++ {
-		if s.clients[i].id == req.ClientId {
-			continue
-		}
-		if err := s.clients[i].stream.Send(msg); err != nil {
-			log.Printf("failed to send to %s: %v", s.clients[i].id, err)
-			s.clients = append(s.clients[:i], s.clients[i+1:]...)
-			i--
-		}
-	}
-
-	return &proto.Ack{Success: true, LogicalTime: s.lTime}, nil
-}
-
-func (s *Server) Leave(ctx context.Context, req *proto.LeaveRequest) (*proto.Ack, error) {
-	s.clientsMu.Lock()
-	var idx int = -1
-	for i, c := range s.clients {
-		if c.id == req.ClientId {
-			idx = i
-			break
-		}
-	}
-	if idx != -1 {
-		s.clients = append(s.clients[:idx], s.clients[idx+1:]...)
-	}
-	s.clientsMu.Unlock()
-
-	if req.LogicalTime > s.lTime {
-		s.lTime = req.LogicalTime
-	}
-	s.lTime++
-
-	s.broadcast(&proto.ChatMessage{
-		From:        "server",
-		Message:     fmt.Sprintf("%s has left the chat", req.ClientId),
-		LogicalTime: s.lTime,
-	}, req.ClientId)
-
-	return &proto.Ack{Success: true, LogicalTime: s.lTime}, nil
-}
